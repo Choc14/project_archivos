912558f350155f60f6850b80cd9366d100fdb19e
choc1403 <eloicx@gmail.com>
2022-08-17 21:28:56 -0600

    arreglado

--- apps/customers/models.py
+++ apps/customers/models.py
@@ -15,20 +15,22 @@ class City(models.Model):
 
     def __str__(self):
         return self.name_city
-
+''' 
     def toJSON(self):
         item = model_to_dict(self)
         return item
+'''
 
 class Id(models.Model):
     id_type = models.CharField(max_length=25)
 
     def __str__(self):
         return self.id_type
-
+'''
     def toJSON(self):
         item = model_to_dict(self)
         return item
+'''
 
 class Customer(models.Model):
     first_name = models.CharField(max_length=25)
@@ -44,7 +46,7 @@ class Customer(models.Model):
 
     def __str__(self):
         return self.first_name + " " + self.last_name
-
+'''
     def toJSON(self):
         item = model_to_dict(self)
         item['date_birth'] = self.date_birth.strftime('%Y-%m-%d')
@@ -52,3 +54,4 @@ class Customer(models.Model):
         item['id_type'] = self.id_type.toJSON()
         item['created_at'] = self.created_at.strftime('%Y-%m-%d')
         return item
+'''
